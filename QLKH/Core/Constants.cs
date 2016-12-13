54fde0f4089789d6b568561ed4c5f2d6d03be254
Thuan Huynh <minhthuan.dev@gmail.com>
2016-12-13 11:15:57 +0700

    update db

---
+++ QLKH/Core/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLKH.Core
+{
+    public class Constants
+    {
+        public enum ERoleCode
+        {
+            Admin = 1,
+            Staff = 2
+        }
+    }
+}
--- QLKH/Login.xaml.cs
+++ QLKH/Login.xaml.cs
@@ -18,11 +18,16 @@ namespace QLKH
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
